34ffee83f942b010666e67cfec1d4104b1f4573a
Anubhav311 <anubhavbhambri1@gmail.com>
2019-04-26 01:52:21 +0530

    added message of the day

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();//reads .env and merges it into process.env
+
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000 //making the port dynamic for heroku deployement
 server.listen(port, () => {
   console.log('\n*** Server Running on http://localhost:4000 ***\n');
 });
